774bd37a747e0e4fe744245b9b7fcc87a7ed42d7
Masamoto Miyata <miyata@sincere-co.com>
2012-10-20 19:53:19 +0900

    Initial Commit

---
+++ TestSlideMenu/SmAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SmAppDelegate.h
+//  TestSlideMenu
+//
+//  Created by Masamoto Miyata on 2012/10/20.
+//  Copyright (c) 2012年 Masamoto Miyata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SmAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestSlideMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestSlideMenu
+//
+//  Created by Masamoto Miyata on 2012/10/20.
+//  Copyright (c) 2012年 Masamoto Miyata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SmAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SmAppDelegate class]));
+    }
+}
